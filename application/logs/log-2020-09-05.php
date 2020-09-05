a3ddbf1a3e9ce3827e205ab8320044c178c8ba2e
mohanarayaz <mohana.rayaz@gmail.com>
2020-09-05 14:04:40 +0530

    first commit

---
+++ application/logs/log-2020-09-05.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-09-05 08:49:58 --> 404 Page Not Found: Uploads/system
+ERROR - 2020-09-05 08:50:21 --> Severity: Warning --> mysqli_connect(): (HY000/1045): Access denied for user 'mona.azhagesan@gmail.com'@'localhost' (using password: YES) D:\xampp\htdocs\Ekattor\application\controllers\Install.php 126
+ERROR - 2020-09-05 08:50:21 --> Severity: Warning --> mysqli_close() expects parameter 1 to be mysqli, boolean given D:\xampp\htdocs\Ekattor\application\controllers\Install.php 128
+ERROR - 2020-09-05 08:51:21 --> Severity: Warning --> mysqli_connect(): (HY000/1045): Access denied for user 'testuserlt4@gmail.com'@'localhost' (using password: YES) D:\xampp\htdocs\Ekattor\application\controllers\Install.php 126
+ERROR - 2020-09-05 08:51:21 --> Severity: Warning --> mysqli_close() expects parameter 1 to be mysqli, boolean given D:\xampp\htdocs\Ekattor\application\controllers\Install.php 128
